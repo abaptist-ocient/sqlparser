d2bd191733af59692e49d1208d126fdf79c4995a
Andrew Baptist <abaptist@ocient.com>
2021-10-18 15:29:26 -0400

    Added Dearborn query

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use std::{fs, io};
-
+use anyhow::Result;
 use sqlparser::dialect::GenericDialect;
 use sqlparser::parser::Parser;
+use std::fs;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<()> {
     let sql = fs::read_to_string("sql.txt")?;
 
     let dialect = GenericDialect {}; // or AnsiDialect, or your own dialect ...
-    let ast = Parser::parse_sql(&dialect, sql.as_str()).unwrap();
+    let ast = Parser::parse_sql(&dialect, sql.as_str())?;
 
     println!("AST: {:?}", ast[0]);
     Ok(())
